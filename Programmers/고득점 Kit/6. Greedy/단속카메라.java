f6de654002097272ead32aa0786cc9d1e44bb522
h-swon <heosw1107@gmail.com>
2022-01-01 06:47:34 +0900

    Create 단속카메라.java

---
+++ Programmers/고득점 Kit/6. Greedy/단속카메라.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        
+        Arrays.sort(routes, (o1, o2)-> o1[1]-o2[1]);
+        int answer = 1;
+        int exit = routes[0][1];
+        for(int i=1; i<routes.length; i++){
+            if(exit < routes[i][0]){
+                exit = routes[i][1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
