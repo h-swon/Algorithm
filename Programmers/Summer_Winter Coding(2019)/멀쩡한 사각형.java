7ca2bdd215b7fb2b588ab8527f9e8640585c563d
h-swon <heosw1107@gmail.com>
2022-03-10 23:43:20 +0900

    Create 멀쩡한 사각형.java

---
+++ Programmers/Summer_Winter Coding(2019)/멀쩡한 사각형.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int w, int h) {
+        long answer = 0;
+        for(int i=0; i<w; i++) {
+            answer += (long)h * i / w;
+        }
+        return answer*2;
+    }
+    
+}
