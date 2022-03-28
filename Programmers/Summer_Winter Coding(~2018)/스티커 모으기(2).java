916bca7fd5dbea215f93549299a78995e9e3db1c
h-swon <heosw1107@gmail.com>
2022-03-29 08:39:17 +0900

    Create 스티커 모으기(2).java

---
+++ Programmers/Summer_Winter Coding(~2018)/스티커 모으기(2).java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int sticker[]) {
+        
+        if(sticker.length == 1) 
+            return sticker[0];
+        
+        int answer = 0;
+        int[] dp1 = new int[sticker.length];
+        int[] dp2 = new int[sticker.length];
+        
+        dp1[0] = sticker[0];
+        dp1[1] = sticker[0];
+        dp2[0] = 0;
+        dp2[1] = sticker[1];
+        
+        for(int i=2; i<sticker.length; i++) {
+            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + sticker[i]);
+            dp2[i] = Math.max(dp2[i-1], dp2[i-2] + sticker[i]);
+        }
+        
+        answer = Math.max(dp1[sticker.length-2], dp2[sticker.length-1]);
+        
+        return answer;
+    }
+}
